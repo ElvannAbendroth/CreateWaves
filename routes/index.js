6db6e00ff98d2b34e2099f94a489d5d04e704e9b
Elvann <elvann.abendroth@gmail.com>
2018-11-02 10:29:22 -0400

    custom template engine working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var templateEngine = require('../models/templateEngine');
+var fs = require('fs');
+var path = require('path');
+
 var router = express.Router();
+
+/*new templateEngine(templateString, pageUrl, title, meta, content)*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  const title = "CreateWaves"
+  const meta = fs.readFileSync(path.join(__dirname, '../views/meta.html'), "utf8");
+  const url = '/';
+  const content = '';
+  
+  const metaTemplate = templateEngine(meta, '/', title, '', '');
 
+  const template = fs.readFileSync(path.join(__dirname, '../views/index.html'), "utf8");
+  const result = templateEngine(template, '/', title, metaTemplate, '');
+  res.send(result);
+});
 module.exports = router;
